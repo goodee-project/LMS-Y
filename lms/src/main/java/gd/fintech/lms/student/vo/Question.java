8cfd380aef97fea6722819f503264f586b8a61de
diffl <diffl@DESKTOP-AHG7B05>
2020-12-14 12:14:36 +0900

    [박창현] 파트별로 세분화 시킨 후 학생파트를 맞아 질문게시판VO생성중

--- lms/src/main/java/gd/fintech/lms/student/vo/Question.java
+++ lms/src/main/java/gd/fintech/lms/student/vo/Question.java
@@ -1,8 +1,13 @@
 package gd.fintech.lms.student.vo;
 
 import lombok.Data;
-
+//학생이 질문을 등록하는 질문 게시판 vo
 @Data
 public class Question {
-
+	
+	private int question_no; // 번호
+	private int lecture_no; // 강좌번호
+	private String account_id; // 학생의 id
+	private String question_writer; // 질문 작성자
+	//진행중~
 }

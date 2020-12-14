712101abd298a1d952e742eda832ac1dd1e8f8d5
kang <kang@rkdgkrl>
2020-12-14 12:22:27 +0900

    [강하기] Faq.java.vo 내용 및 주석 추가

--- lms/src/main/java/gd/fintech/lms/manager/vo/Faq.java
+++ lms/src/main/java/gd/fintech/lms/manager/vo/Faq.java
@@ -2,7 +2,18 @@ package gd.fintech.lms.manager.vo;
 
 import lombok.Data;
 
+// FAQ  관련 vo
 @Data
 public class Faq {
+  private int faqNo; //  FAQ 번호
+  private String accountId;  //계정 id 
+  private String faqWriter; //FAQ 작성자
+  private String faqTitle;  // FAQ 제목
+  private String faqContent;  // FAQ  내용
+  private String faqCreatedate;// FAQ 작성시간 
+  private String faqUpdatedate;// FAQ 수정 시간
+  private int faqCount; // FAQ 조회수
+  
+  
 
 }

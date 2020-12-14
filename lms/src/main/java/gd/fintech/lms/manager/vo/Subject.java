8d843f699372c5573ba9df6885385a97df251b53
Moon <Moon@DESKTOP-9F62M9S>
2020-12-14 12:20:36 +0900

    [문지웅] Subject.java vo 주석 추가

--- lms/src/main/java/gd/fintech/lms/manager/vo/Subject.java
+++ lms/src/main/java/gd/fintech/lms/manager/vo/Subject.java
@@ -2,7 +2,13 @@ package gd.fintech.lms.manager.vo;
 
 import lombok.Data;
 
+// 과목 VO: 과목의 정보 VO
 @Data
 public class Subject {
-
+	private int subjectNo; // 과목 고유번호
+	private String subjectName; // 과목 이름
+	private String subjectCreatedate; // 과목 생성일자
+	private String subjectUpdatedate; // 과목 수정일자
+	private int subjectTotalday; // 과목 총일수
+	private String subjectInfo; // 과목 정보
 }

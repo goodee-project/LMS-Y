a75ca47010fc3895a263586efda960aa2d656740
baebae98 <piskk74@naver.com>
2020-12-15 15:11:04 +0900

    [배형준] 자료실 첨부파일 Mapper 추가 및 오류 수정
    -LectureArchiveFileMapper
     -자료실 첨부파일 목록,입력,삭제,자료실 삭제 추가
    -LectureArchiveMapper.xml
     -경로 설정 오류 수정

--- lms/src/main/java/gd/fintech/lms/dto/LectureArchiveForm/LectureArchiveForm.java
+++ lms/src/main/java/gd/fintech/lms/dto/LectureArchiveForm/LectureArchiveForm.java
@@ -9,8 +9,10 @@ import lombok.Data;
 //Multipart 처리를 위한 LectureArhiveForm VO
 @Data
 public class LectureArchiveForm {
-	private int lectureArchiveNo; //강좌 자료실 고유번호
-	private String accountId; //계정 아이디(강사)
+	private int lectureArchiveNo;//강좌 자료실 고유번호
+	private int lectureNo;//강좌 고유번호
+	private String accountId;//계정 아이디(강사)
+	private String lectureArchiveWriter;//작성자 이름(강사)
 	private String lectureArchiveTitle;//강좌 자료실 제목
 	private String lectureArchiveContent;//강좌 자료실 내용
 	private String lectureArchiveCreateDate;//강좌 자료실 정보 생성 일시
---
+++ lms/src/main/java/gd/fintech/lms/teacher/mapper/LectureArchiveFileMapper.java
@@ -0,0 +1,16 @@
+package gd.fintech.lms.teacher.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import gd.fintech.lms.teacher.vo.LectureArchiveFile;
+
+//자료실 첨부 파일 Mapper
+@Mapper
+public interface LectureArchiveFileMapper {
+	List<String> selectLectureArchiveFileList(int lectureArchiveNo);//자료실 첨부 파일 목록
+	int insertLectureArchiveFile(LectureArchiveFile lectureArchiveFile);//자료실 첨부 입력(추가)
+	int deleteLectureArchiveFile(String lectureArchiveFileUUID);//파일 삭제
+	int deleteLectureArchiveFileOne(int lectureArchiveNo);//파일 첨부된 자료실 삭제
+}

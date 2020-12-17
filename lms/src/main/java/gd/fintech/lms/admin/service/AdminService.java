e9dde5ecb54805190d592f81ef2e37783ef1f2ab
moonlight0229 <vhrvhdhkdrnr@naver.com>
2020-12-17 15:17:47 +0900

    [문지웅]코드 수정 및 주석 수정
    AdminService.java
    - 코드 추가 작성
    - 주석 수정
    ManagerService.java
    - 코드 추가 작성
    
    ManagerQueueMapper.java
    - 코드 오타 수정

--- lms/src/main/java/gd/fintech/lms/admin/service/AdminService.java
+++ lms/src/main/java/gd/fintech/lms/admin/service/AdminService.java
@@ -4,23 +4,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import gd.fintech.lms.admin.mapper.AdminMapper;
-import gd.fintech.lms.admin.vo.Admin;
-import gd.fintech.lms.admin.vo.ManagerQueue;
+import gd.fintech.lms.admin.mapper.ManagerQueueMapper;
+import gd.fintech.lms.manager.mapper.ManagerMapper;
 
 // 관리자가 하는 업무를 위한 서비스
 
 @Service
 @Transactional
 public class AdminService {
-	@Autowired private ManagerQueue managerQueue;
+	@Autowired private ManagerQueueMapper managerQueueMapper;
+//	@Autowired private ManagerMapper managerMapper;
+//	@Autowired private AccountMapper accountMapper;
 		
-	// 관리자가 운영자의 회원가입을 승인하는 메소드
+	// 관리자가 운영자의 회원가입을 승인하는 서비스 메소드
 	// 매개변수: 운영자 아이디
-	// 리턴값:
 	// 운영자의 개인정보를 Manager에 저장
 	// 운영자의 개인정보을 ManagerQueue에서 삭제
+	// 운영자의 계정의 활성화 여부를 활성화로 변경
 	public void approveManagerMembership(String accountId) {
-		
+		// 아이디에 해당하는 운영자의 개인정보를 Manager에 입력
+//		managerMapper.insertManager(accountId);
+		// 아이디에 해당하는 운영자의 개인정보를 ManagerQueue에서 삭제 
+		managerQueueMapper.deleteManagerQueue(accountId);
+		// 아이디에 해당하는 계정의 활성화 여부를 활성화로 변경
+//		AccountMapper.updateAccountState(accountId)
 	}
 }
--- lms/src/main/java/gd/fintech/lms/manager/service/ManagerService.java
+++ lms/src/main/java/gd/fintech/lms/manager/service/ManagerService.java
@@ -1,8 +1,21 @@
 package gd.fintech.lms.manager.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import gd.fintech.lms.manager.mapper.TextbookMapper;
+import gd.fintech.lms.manager.vo.Textbook;
+
+// 운영자가 하는 업무를 위한 서비스
 
 @Service
 public class ManagerService {
-
+	@Autowired TextbookMapper textbookMapper;
+	
+	// 운영자가 교재 정보를 등록하는 서비스 메소드
+	// 매개변수: textbook
+	// 리턴값: 
+	public int addTextbook(Textbook textbook) {
+		return textbookMapper.insertTextbook(textbook);
+	}
 }

e7157d9b68059d8015170a8913a05a72ec6011db
diffl <diffl@DESKTOP-AHG7B05>
2020-12-17 13:39:23 +0900

    [박창현] studentMapper xml,java 수정

--- lms/src/main/java/gd/fintech/lms/student/mapper/StudentMapper.java
+++ lms/src/main/java/gd/fintech/lms/student/mapper/StudentMapper.java
@@ -11,8 +11,8 @@ import gd.fintech.lms.student.vo.Student;
 public interface StudentMapper {
 	//학생 상세보기(정보보기)
 	//매개변수: 
-	//리턴값: 학생들의 모든 정보(id,email,name,phone,gender,birth,address,image,accessdate)
-	List<Student> selectStudentOne(String accountId);
+	//리턴값: 학생의 모든 정보(id,email,name,phone,gender,birth,address,image,accessdate)
+	Student selectStudentOne(String accountId);
 	
 	//학생 정보 수정 폼
 	//매개변수: 학생Vo값

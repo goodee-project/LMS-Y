8cdac144cbf44edefe293168bc05b6cdfcc6f7b6
1F24DCA <fstflr446712@gmail.com>
2020-12-17 17:02:23 +0900

    [신동한] 개발/배포환경별 첨부파일 절대경로를 자동 지정해주는 유틸리티 클래스 FileDist 뼈대 생성
    개발환경 및 배포환경에서 서로 달라지는 절대경로에 대해 하드코딩을 하면 매번 번거롭게 수정해야하기에, 이를 static 메서드를
    통해 받아올 수 있게 함
    
    아직 틀만 만들어둔 상태, 완성해야 함

---
+++ lms/src/main/java/gd/fintech/lms/FileDest.java
@@ -0,0 +1,11 @@
+package gd.fintech.lms;
+
+// 첨부파일의 경로를 자동으로 잡아주도록 도와주는 유틸리티 클래스
+
+public class FileDest {
+	// 첨부파일이 저장될 절대경로를 출력
+	// 리턴값: 첨부파일이 저장될 절대경로(C:\\workspace\\LMS-Y\\src\\main\\resources\\upload\\ or /home/ubuntu/tomcat9/webapps/ROOT/upload/)
+	public static String getFileDist() {
+		return "";
+	}
+}

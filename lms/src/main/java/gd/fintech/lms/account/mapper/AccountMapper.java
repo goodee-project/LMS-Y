3ef1528988db57a8dd15cfde00938d6a2f34ebec
Shin <Shin@Lenovo-Slim3>
2020-12-18 11:35:58 +0900

    [신민준] AccountMapper에서 계정상태 메소드명 수정 및 계정상태 탈퇴 메소드 추가
    1. AccountMapper에 계정상태여부 메소드명 변경 및 추가
    - updateAccountStateActiveByAccountId(활성화 변경)
    - updateAccountStateInvalidByAccountId(탈퇴 변경)
    2. CareerMapper 메소드 및 쿼리 추가
    - selectCareerAll(모든 경력 조회)
    - selectCareerByAccountId(계정에 따른 경력 조회)
    - insertCareer(경력 입력)
    - deleteCareerByAccountId(계정의 모든 경력삭제)
    - deleteCareerByCareerNo(경력 한 단위로 삭제)

--- lms/src/main/java/gd/fintech/lms/account/mapper/AccountMapper.java
+++ lms/src/main/java/gd/fintech/lms/account/mapper/AccountMapper.java
@@ -1,10 +1,15 @@
 package gd.fintech.lms.account.mapper;
 
+
+import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import gd.fintech.lms.account.vo.Account;
 
 // 회원 계정 등록 정보에 관한 인터페이스(로그인 기능)
+// Account(계정) 테이블에서 데이터를 삭제하면 안 되는 것으로 규정(상태만 변경)
 
 @Mapper
 public interface AccountMapper {
@@ -13,8 +18,18 @@ public interface AccountMapper {
 	// 리턴값: 등록된 계정 정보인지 여부를 조회하여 반환
 	Account selectMemberById(Account account);
 	
-	// Account 테이블에서 계정 상태를 수정하는 메소드
+	// Account 테이블에 등록된 모든 계정을 조회하는 메소드
+	// 매개변수: 페이징을 위한 시작페이지, 한페이지에 출력되는 행의 수(Integer 형으로 받음)
+	// 리턴값: 모든 계정을 조회한 결과리스트
+	List<Account> selectMemberAll(Map<String, Integer> map);
+	
+	// Account 테이블에서 계정 상태를 활성화로 수정하는 메소드
 	// 매개변수: 계정 ID
-	// 리턴값: 계정 활성화 수정한 행
-	int updateAccountStateByAccountId(Account account);
+	// 리턴값: 계정상태를 활성화로 수정한 행
+	int updateAccountStateActiveByAccountId(String accountId);
+	
+	// Account 테이블에서 계정 상태를 탈퇴로 수정하는 메소드
+	// 매개변수: 계정 ID
+	// 리턴값: 계정상태를 탈퇴로 수정한 행
+	int updateAccountStateInvalidByAccountId(String accountId);
 }
--- lms/src/main/java/gd/fintech/lms/account/mapper/CareerMapper.java
+++ lms/src/main/java/gd/fintech/lms/account/mapper/CareerMapper.java
@@ -1,5 +1,8 @@
 package gd.fintech.lms.account.mapper;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import gd.fintech.lms.account.vo.Career;
@@ -8,8 +11,28 @@ import gd.fintech.lms.account.vo.Career;
 
 @Mapper
 public interface CareerMapper {
-	// 계정 ID별 경력을 조회하는 메소드
+	// 모든 계정의 경력 목록을 조회하는 메소드
+	// 매개변수: 페이징을 위한 시작페이지, 한페이지에서 보여주는 행의 수
+	// 리턴값: 모든 계정에 경력 정보 조회 결과값
+	List<Career> selectCareerAll(Map<String, Integer> map);
+	
+	// 계정 ID별 경력 목록을 조회하는 메소드
 	// 매개변수: 계정의 ID 값
 	// 리턴값: ID에 따른 경력 정보 조회 결과값
-	Career selectCareerByAccountId(String accountId);	
+	List<Career> selectCareerByAccountId(String accountId);
+	
+	// 경력 내용을 입력하기 위한 메소드
+	// 매개변수: 계정Id, 경력내용, 경력시작일, 경력마지막일
+	// 리턴값: 경력을 입력한 행의 수
+	int insertCareer(Career career);
+	
+	// 해당 계정 ID의 모든 경력을 삭제하는 메소드
+	// 매개변수: 계정의 ID 값
+	// 리턴값: ID에 따른 경력을 삭제한 행
+	int deleteCareerByAccountId(String accountId);
+	
+	// 경력 내용 하나만 삭제하는 메소드
+	// 매개변수: 해당 경력에 해당하는 경력번호
+	// 리턴값: 한 행의 경력을 지운 행
+	int deleteCareerByCareerNo(int careerNo);
 }

962890b0f3ff1c4e8a79f2d6e80ed2df7450764b
moonlight0229 <vhrvhdhkdrnr@naver.com>
2020-12-23 16:42:23 +0900

    [문지웅]ManagerService.java 메소드 삭제
    ManagerService.java
    - Service 분리를 위해 createSubject 메소드 삭제
    - Service 분리를 위해 createTextbook 메소드 삭제
    - Service 분리를 위해 modifyTextbook 메소드 삭제

--- lms/src/main/java/gd/fintech/lms/manager/service/ManagerService.java
+++ lms/src/main/java/gd/fintech/lms/manager/service/ManagerService.java
@@ -31,28 +31,6 @@ public class ManagerService {
 		return syllabusMapper.updateSyllabusManagerSign(syllabusNo);
 	}
 	
-	// 운영자가 과목 정보를 입력하는 서비스 메소드
-	// 매개변수:
-	// 리턴값:
-	public int createSubject(Subject subject) {
-		return subjectMapper.insertSubject(subject);
-	}
-	
-	// 운영자가 교재 정보를 등록하는 서비스 메소드
-	// 매개변수: textbook
-	// 리턴값: 
-	public int createTextbook(Textbook textbook) {
-		return textbookMapper.insertTextbook(textbook);
-	}
-	
-	// 운영자가 교재 정보를 수정하는 서비스 메소드
-	// 매개변수: 
-	// 리턴값: 
-	public int modifyTextbook(Textbook textbook) {
-		return textbookMapper.updateTextbook(textbook);
-	}
-	
-	
 	// 운영자의 개인 정보를 볼수있는 서비스 메소드 
 	// 매개변수: 운영자의 계정 ID
 	// 리턴값: 운영자의 상세 정보 

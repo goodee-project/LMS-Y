93ef08fc6d5ed6aaac6d3acff16ed37ae267112e
kang <kang@rkdgkrl>
2020-12-31 09:55:30 +0900

    [강하기] 강좌Mapper.java수정
    LectureManagerMapper.java수정

--- lms/src/main/java/gd/fintech/lms/manager/mapper/LectureManagerMapper.java
+++ lms/src/main/java/gd/fintech/lms/manager/mapper/LectureManagerMapper.java
@@ -5,7 +5,11 @@ import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import gd.fintech.lms.manager.vo.Classroom;
 import gd.fintech.lms.manager.vo.Lecture;
+import gd.fintech.lms.manager.vo.Subject;
+import gd.fintech.lms.manager.vo.Textbook;
+import gd.fintech.lms.teacher.vo.Syllabus;
 
 // 운영자의 강좌에 대한 메퍼 인터페이스
 
@@ -15,6 +19,22 @@ public interface LectureManagerMapper {
 	// 매개변수:  
 	// 리턴값: 강좌의 목록
 	List<Lecture> selectLectureListByPage(Map<String, Integer> map);  
+	
+	// 강좌에서의 강의실 리스트
+	// 리턴값: 강의실 목록
+	List<Classroom> selectLectureClassroomList();
+	
+	// 강좌에서의 강의계획서 리스트
+	// 리턴값: 강의계획서 목록
+	List<Syllabus> selectLectureSyllabusList();
+	
+	// 강좌에서의 교재 리스트
+	// 리턴값: 교재목록
+	List<Textbook> selectLectureTextbookList();
+	
+	// 강좌에서의 과목 리스트
+	// 리턴값: 과목 목록
+	List<Subject> selectLectureSubjectList();
 	
 	// 강좌 개수
 	// 리턴값: 행의 총 개수
@@ -40,11 +60,9 @@ public interface LectureManagerMapper {
 	// 리턴값: 행의 상세정보
 	Lecture selectLectureDetail(int lectureNo); 
 	
-	
 	// accountId 를 이용한 강좌 리스트
 	// 매개변수: accountId
 	// 리턴값: accountId에 해당하는 강좌 리스트
 	List<Lecture> selectTeacherLectureDetail(String accountId);
-
 
 }

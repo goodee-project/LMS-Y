997d7e8df7afbe7b8683cbab7019fa5aec3a6161
Shin <Shin@Lenovo-Slim3>
2021-01-12 11:29:45 +0900

    [신민준] 스케줄러 수정, 통계를 위한 폴더 생성
    1. 관리자는 휴면 계정 전환 안 되도록 쿼리 수정
    2. AccountScheduler 클래스 위치 수정
    3. Lms 애플리케이션 파일에 애노테이션 추가
    4. 통계를 위한 폴더 및 뷰 샘플 파일 생성

--- lms/src/main/java/gd/fintech/lms/AccountScheduler.java
+++ lms/src/main/java/gd/fintech/lms/AccountScheduler.java
@@ -1,22 +1,20 @@
-package gd.fintech.lms.account.controller;
+package gd.fintech.lms;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 
 import gd.fintech.lms.account.service.AccountService;
 
-// 휴면 계정을 관리하기 위한 스케줄 기능의 컨트롤러
+// 휴면 계정을 관리하기 위한 스케줄 기능의 클래스
 
-@EnableScheduling
 @Component
 public class AccountScheduler {
 	// AccountService 객체 주입
-	@Autowired private AccountService accountService;
+	@Autowired AccountService accountService;
 	
 	Logger logger =  LoggerFactory.getLogger(AccountScheduler.class);
 	
--- lms/src/main/java/gd/fintech/lms/LmsApplication.java
+++ lms/src/main/java/gd/fintech/lms/LmsApplication.java
@@ -3,13 +3,14 @@ package gd.fintech.lms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ServletComponentScan	// web필터를 사용하기 위한 애노테이션 추가
+@EnableScheduling		// 스케줄 작업을 위한 애노테이션 추가
 public class LmsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LmsApplication.class, args);
 	}
-
 }
